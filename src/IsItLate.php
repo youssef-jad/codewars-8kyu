8f7fff57e205cbf2add00f505f95940dcef1fd8e
Youssef Jad <youssef.j4d@gmail.com>
2019-04-30 04:14:09 +0200

    submit the code

--- src/IsItLate.php
+++ src/IsItLate.php
@@ -4,5 +4,10 @@ namespace Petunia\Kyu;
 class IsItLate
 {
 
+    public function periodIsLate($last , $today , $period )
+    {
+        $interval = $today->diff($last);
+        return $interval->days > $period ? true : false ;
+    }
 
 }
--- tests/TestMain.php
+++ tests/TestMain.php
@@ -7,11 +7,8 @@ class TestMain extends TestCase
 {
 
     public function testExample() {
-//        $isItlate = new IsItLate();
-        $this->assertSame(false, false);
-//        $this->assertSame(false, $isItlate->periodIsLate(new DateTime('2016-07-13'), new DateTime('2016-08-16'), 35));
-//        $this->assertSame(true, $isItlate->periodIsLate(new DateTime('2016-07-13'), new DateTime('2016-08-16'), 28));
+        $initClass = new IsItLate();
+        $this->assertSame(false, $initClass->periodIsLate(new DateTime('2016-07-13'), new DateTime('2016-08-16'), 35));
+        $this->assertSame(true, $initClass->periodIsLate(new DateTime('2016-07-13'), new DateTime('2016-08-16'), 28));
     }
-
-
 }
